638ee4e6a7f1414181099c1705a48dcc1a368f0a
Donald Chinhuru <donychinhuru@gmail.com>
2019-08-19 23:41:54 +0200

    update

--- BulkSmsApi/__init__.py
+++ BulkSmsApi/__init__.py
@@ -1,6 +1,3 @@
-from BulkSmsApi import Client
-from BulkSmsApi import Parse
-
 __name__    = "BulkSmsApi"
-__version__ = "1.2.0"
-__author__  = "Donald Chinhuru"
+__version__ = "1.0.0"
+__author__  = ""
